b8218255877ee236eacadd17790170cfced238f5
nedam <neda.masiha>
2020-06-01 19:41:55 +1000

    remoing commented code

--- Learning/Learning/Startup.cs
+++ Learning/Learning/Startup.cs
@@ -19,7 +19,7 @@ namespace Learning
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        // this is test
+  
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
